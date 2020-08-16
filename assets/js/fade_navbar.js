8a0284c6dbb9d9ab274b92a902ae8902cc6bce99
nahuelbrandan <nahuelbrandan123@gmail.com>
2020-08-16 18:01:19 -0300

    show navbar when scroll up in posts

--- assets/js/fade_navbar.js
+++ assets/js/fade_navbar.js
@@ -1,16 +1,17 @@
-(function ($) {
-    $(document).ready(function () {
+// add padding top to show content behind navbar
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 
-        // fade in .navbar
-        $(function () {
-            $(window).scroll(function () {
-                // set distance user needs to scroll before we fadeIn navbar
-                if ($(this).scrollTop() > 600) {
-                    $('.navbar').fadeOut();
-                } else {
-                    $('.navbar').fadeIn();
-                }
-            });
-        });
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
     });
-}(jQuery));
+}
